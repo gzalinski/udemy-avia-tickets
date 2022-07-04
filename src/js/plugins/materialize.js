257c41b7060600709a2e102041a6d4a522237a4a
Gheorghe <gheorghe@enspiredev.com>
2022-07-04 18:15:07 +0300

    meterialize init select & add method getSelectInstance

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -1,3 +1,11 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 
+
+//Init select
+const select = document.querySelectorAll('select')
+M.FormSelect.init(select)
+
+export function getSelectInstance(el){
+    return M.FormSelect.getInstance(el)
+}
