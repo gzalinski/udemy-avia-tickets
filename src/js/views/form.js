89d71333d03a749588aace31c34679785f183575
Gheorghe <gheorghe@enspiredev.com>
2022-07-04 20:40:15 +0300

    class formUI define elements

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -1,6 +1,10 @@
 class FormUI {
     constructor() {
-      
+        this.$form = document.forms['locationControls'];
+        this.origin = document.getElementById('autocomplete-origin');
+        this.destination = document.getElementById('autocomplete-destination');
+        this.depart = document.getElementById('datepicker-depart');
+        this.return = document.getElementById('datepicker-return');
     }
   
   }
