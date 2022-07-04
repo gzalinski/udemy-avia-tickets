d6f61466cc59c58f42d71217793b938e9fd3cd87
Gheorghe <gheorghe@enspiredev.com>
2022-07-04 20:58:38 +0300

    add method initApp(). Get locations from api and set to materialize autocomplete

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 import  "../css/style.css";
 import './plugins';
 import locations from "./store/locations";
+import formUI from './views/form';
 
-locations.init().then(res => {
-    console.log(locations.getCitiesByCountryCode('PE'))
-})
+
+document.addEventListener('DOMContentLoaded', e => {
+  
+  // Events
+  initApp();
+ 
+
+  // handlers
+  async function initApp() {
+    await locations.init();
+
+    console.log(locations)
+    
+    formUI.setAutocompleteData(locations.shortCities);
+  }
+
+  
+});
