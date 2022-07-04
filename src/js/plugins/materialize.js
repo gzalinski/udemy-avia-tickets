54e9015e15b2532b6afbd18a245507459a53bc9b
Gheorghe <gheorghe@enspiredev.com>
2022-07-04 18:16:31 +0300

    meterialize init autocomplete & add method getAutocompleteInstance

--- src/js/plugins/materialize.js
+++ src/js/plugins/materialize.js
@@ -9,3 +9,17 @@ M.FormSelect.init(select)
 export function getSelectInstance(el){
     return M.FormSelect.getInstance(el)
 }
+
+//Init autocomplete
+const autocomplete =  document.querySelectorAll('.autocomplete')
+M.Autocomplete.init(autocomplete, {
+    data: {
+        Apple: null,
+        Microsfot: null,
+        Google: 'https://placehold.it/250x250'
+    }
+})
+
+export function getAutocompleteInstance(el){
+    return M.Autocomplete.getInstance(el)
+}
