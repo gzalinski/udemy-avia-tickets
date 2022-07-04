b1924ae5323123b7bc492cc60070821989e647d1
Gheorghe <gheorghe@enspiredev.com>
2022-07-04 20:45:27 +0300

    class formUI import & define materialize ui instances for autocomplet and datepicker

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -1,14 +1,23 @@
+import { getAutocompleteInstance, getDatePickerInstance } from '../plugins/materialize';
 class FormUI {
-    constructor() {
+    constructor(autocompleteInstance, datePickerInstance) {
+        //elements
         this.$form = document.forms['locationControls'];
         this.origin = document.getElementById('autocomplete-origin');
         this.destination = document.getElementById('autocomplete-destination');
         this.depart = document.getElementById('datepicker-depart');
         this.return = document.getElementById('datepicker-return');
+
+        //materialize ui instances
+        this.originAutocomplete = autocompleteInstance(this.origin);
+        this.destinationAutocomplete = autocompleteInstance(this.destination);
+        this.departDatePicker = datePickerInstance(this.depart);
+        this.returnDatePicker = datePickerInstance(this.return);
+
     }
   
   }
   
-  const formUI = new FormUI();
+  const formUI = new FormUI(getAutocompleteInstance, getDatePickerInstance);
   
   export default formUI;
